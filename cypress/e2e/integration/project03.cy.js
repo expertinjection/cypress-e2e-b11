7e44bb56cf4d1a9acb50eff28d527748b0914056
expertinjection <mr.haithamiq@gmail.com>
2025-05-20 16:02:05 -0500

    project 3

--- cypress/e2e/integration/project03.cy.js
+++ cypress/e2e/integration/project03.cy.js
@@ -1,8 +1,4 @@
 /// <reference types="cypress"/>
-import LoginPage from "../../pages/LoginPage";
-
-const loginPage = new LoginPage();
-
 describe("Cypress03 project", () => {
   beforeEach(() => {
     cy.visit("https://www.techglobal-training.com/frontend/booking");
@@ -96,6 +92,70 @@ describe("Cypress03 project", () => {
       .and("be.enabled");
   });
   it('Test Case 03 - Validate the booking for 1 passenger and one way', () =>{
-    
+    cy.get('input[value="One way"]').check()
+    cy.get('select').eq(0).select('Business')
+    cy.get('select').eq(1).select('Illinois')
+    cy.get('select').eq(2).select('Florida')
+    cy.get('input[placeholder="MM/DD/YY"]').eq(0).realClick()
+    cy.get('.react-datepicker-ignore-onclickoutside')
+    cy.get('div[aria-label="Choose Tuesday, May 27th, 2025"]').realClick()
+    cy.get('select').eq(3).select('1')
+    cy.get('select').eq(4).select('Senior (65+)')
+    cy.get('.Button_c_button__TmkRS').click()
+
+    cy.get('.ml-3').find('h1').should('contain', 'DEPART');
+    cy.get('.ml-3').find('h3').eq(0).should('contain', 'IL to FL'); 
+    cy.get('.ml-3').find('p').eq(0).should('contain', 'Tue May 27 2025');
+    cy.get('.ml-3').find('p').eq(1).should('contain', 'Number of Passengers: 1');
+    cy.get('.ml-3').find('p').eq(2).should('contain', 'Passenger 1: Senior (65+)');
+    cy.get('.ml-3').find('p').eq(3).should('contain', 'Cabin class: Business');
+
+  });
+  it('Test Case 04 - Validate the booking for 1 passenger and round trip', () => {
+    cy.get('input[value="Round trip"]').check()
+    cy.get('select').eq(0).select('First')
+    cy.get('select').eq(1).select('California')
+    cy.get('select').eq(2).select('Illinois')
+    cy.get('input[placeholder="MM/DD/YY"]').eq(0).realClick()
+    cy.get('.react-datepicker-ignore-onclickoutside')
+    cy.get('div[aria-label="Choose Tuesday, May 27th, 2025"]').realClick()
+    cy.get('input[placeholder="MM/DD/YY"]').eq(1).realClick()
+    cy.get('button[aria-label="Next Month"]').realClick()
+    cy.get('div[aria-label="Choose Friday, June 27th, 2025"]').realClick()
+    cy.get('select').eq(3).select('1')
+    cy.get('select').eq(4).select('Adult (16-64)')
+    cy.get('.Button_c_button__TmkRS').click()
+
+    cy.get('.ml-3').find('h1').eq(0).should('contain', 'DEPART');
+    cy.get('.ml-3').find('h1').eq(1).should('contain', 'RETURN');
+    cy.get('.ml-3').find('h3').eq(0).should('contain', 'CA to IL'); 
+    cy.get('.ml-3').find('h3').eq(1).should('contain', 'IL to CA'); 
+
+    cy.get('.ml-3').find('p').eq(0).should('contain', 'Tue May 27 2025');
+    cy.get('.ml-3').find('p').eq(1).should('contain', 'Fri Jun 27 2025');
+    cy.get('.ml-3').find('p').eq(2).should('contain', 'Number of Passengers: 1');
+    cy.get('.ml-3').find('p').eq(3).should('contain', 'Passenger 1: Adult (16-64)');
+    cy.get('.ml-3').find('p').eq(4).should('contain', 'Cabin class: First');
   })
-});
+  it('Test Case 05 - Validate the booking for 2 passengers and one way', () => {
+    cy.get('input[value="One way"]').check()
+    cy.get('select').eq(0).select('Premium Economy')
+    cy.get('select').eq(1).select('New York')
+    cy.get('select').eq(2).select('Texas')
+    cy.get('input[placeholder="MM/DD/YY"]').eq(0).realClick()
+    cy.get('.react-datepicker-ignore-onclickoutside')
+    cy.get('div[aria-label="Choose Wednesday, May 21st, 2025"]').realClick()
+    cy.get('select').eq(3).select('2')
+    cy.get('select').eq(4).select('Adult (16-64)')
+    cy.get('select').eq(5).select('Child (2-11)')
+    cy.get('.Button_c_button__TmkRS').click()
+
+    cy.get('.ml-3').find('h1').should('contain', 'DEPART');
+    cy.get('.ml-3').find('h3').eq(0).should('contain', 'NY to TX'); 
+    cy.get('.ml-3').find('p').eq(0).should('contain', 'Wed May 21 2025');
+    cy.get('.ml-3').find('p').eq(1).should('contain', 'Number of Passengers: 2');
+    cy.get('.ml-3').find('p').eq(2).should('contain', 'Passenger 1: Adult (16-64)');
+    cy.get('.ml-3').find('p').eq(3).should('contain', 'Passenger 2: Child (2-11)');
+    cy.get('.ml-3').find('p').eq(4).should('contain', 'Cabin class: Premium Economy');
+  })
+})
